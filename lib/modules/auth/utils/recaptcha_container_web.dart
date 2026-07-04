c6171efb3b044c058ca2e409b8207dda0b3c8d11
Sivakesh Raman <info@sivakesh.com>
2026-07-04 18:21:49 +0530

    Fix prod auth error

--- lib/modules/auth/utils/recaptcha_container_web.dart
+++ lib/modules/auth/utils/recaptcha_container_web.dart
@@ -1,16 +1,13 @@
 // ignore_for_file: deprecated_member_use
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
-import 'dart:ui' as ui;
-
-import 'package:flutter/widgets.dart';
+import 'dart:ui_web' as ui_web;
 
 void registerRecaptchaContainer() {
   // Ignore duplicate registration if hot reload or rebuild runs again.
   // The browser factory only needs to exist once.
   try {
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(
+    ui_web.platformViewRegistry.registerViewFactory(
       'recaptcha-container',
       (int viewId) => html.DivElement()..id = 'recaptcha-container',
     );
